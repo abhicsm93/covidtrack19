a6004ff77a83487129b0f2176b4d0e38be9109e3
mahajanc <chirayushrikant.mahajan@infineon.com>
2020-04-26 21:09:49 +0530

    Updates

---
+++ precache-manifest.208472c32f9910722030136fd899251f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d329e9b00a6eee996b7283bf7c71d4eb",
+    "url": "/covidtrack19/index.html"
+  },
+  {
+    "revision": "c51902ec3215337314ce",
+    "url": "/covidtrack19/static/css/main.f8181d50.chunk.css"
+  },
+  {
+    "revision": "0e276793a3dcdd8eaaea",
+    "url": "/covidtrack19/static/js/2.63f1563d.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/covidtrack19/static/js/2.63f1563d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c51902ec3215337314ce",
+    "url": "/covidtrack19/static/js/main.86b2f8eb.chunk.js"
+  },
+  {
+    "revision": "3aed87df1f3aed685002",
+    "url": "/covidtrack19/static/js/runtime-main.0f799f73.js"
+  }
+]);
